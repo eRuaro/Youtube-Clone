c12343c30da37d54b9d87bb04d02aa37c7e4164f
Neil Ruaro <erickson_ruaroii@dlsu.edu.ph>
2021-01-07 10:28:46 +0800

    Added App Bar widget

--- lib/videos_post.dart
+++ lib/videos_post.dart
@@ -14,35 +14,38 @@ class YoutubePost extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Row(
-          children: [
-            Image.network(
-                'https://i.ytimg.com/an_webp/EE3k9EH0p6Y/mqdefault_6s.webp?du=3000&sqp=CLLM1f8F&rs=AOn4CLDaPNQt9KYvz-_WnyFV3_GhkBvn0Q')
-          ],
-        ),
-        Row(
-          children: [
-            Column(
-              children: [Icon(Icons.account_circle_rounded)],
-            ),
-            Column(
-              children: [
-                Row(
-                  children: [Text('LA LAKERS VS MEMPHIS GRIZZLIES')],
-                ),
-                Row(
-                  children: [Text('NBA')],
-                ),
-                Row(
-                  children: [Text('697K Views * 5 Hours Ago')],
-                )
-              ],
-            ),
-          ],
-        )
-      ],
+    return Container(
+      margin: EdgeInsets.all(10.0),
+      child: Column(
+        children: [
+          Row(
+            children: [
+              Image.network(
+                  'data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7')
+            ],
+          ),
+          Row(
+            children: [
+              Column(
+                children: [Icon(Icons.account_circle_rounded)],
+              ),
+              Column(
+                children: [
+                  Row(
+                    children: [Text('LA LAKERS VS MEMPHIS GRIZZLIES')],
+                  ),
+                  Row(
+                    children: [Text('NBA')],
+                  ),
+                  Row(
+                    children: [Text('697K Views * 5 Hours Ago')],
+                  )
+                ],
+              ),
+            ],
+          )
+        ],
+      ),
     );
   }
 }
