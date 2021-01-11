8348ef4893ce0fcc2444c710cea1549ddba17f1d
Neil Ruaro <erickson_ruaroii@dlsu.edu.ph>
2021-01-11 18:53:17 +0800

    Added Dark Mode

--- lib/main.dart
+++ lib/main.dart
@@ -21,12 +21,15 @@ class MyApp extends StatelessWidget {
           // or simply save your changes to "hot reload" in a Flutter IDE).
           // Notice that the counter didn't reset back to zero; the application
           // is not restarted.
-          primarySwatch: Colors.blue,
+
           // This makes the visual density adapt to the platform that you run
           // the app on. For desktop platforms, the controls will be smaller and
           // closer together (more dense) than on mobile platforms.
           visualDensity: VisualDensity.adaptivePlatformDensity,
-          backgroundColor: Colors.black,
+        ),
+        darkTheme: ThemeData(
+          brightness: Brightness.dark,
+          visualDensity: VisualDensity.adaptivePlatformDensity,
         ),
         home: HomePage());
   }
